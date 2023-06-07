364d9a3612d79ff19fb4319d9dd130b5960c3f81
Micah Njeru <micahnjeru1@gmail.com>
2023-06-07 08:23:21 +0300

    Added a CoreData model

---
+++ Experience/DataController.swift
@@ -0,0 +1,13 @@
+//
+//  DataController.swift
+//  Experience
+//
+//  Created by Micah Jesse Njeru on 07/06/2023.
+//
+
+import CoreData
+import Foundation
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "Experience")
+}
