bd43a08d8dec4677f06c86135698c9f062f39aa9
Micah Njeru <micahnjeru1@gmail.com>
2023-06-07 07:53:25 +0300

    Initial commit

---
+++ Experience/ExperienceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExperienceApp.swift
+//  Experience
+//
+//  Created by Micah Jesse Njeru on 06/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct ExperienceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
